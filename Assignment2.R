f1350c0f9f0b711358f952c30f4f58f1c02bd72f
z251 <70263330+z251@users.noreply.github.com>
2020-09-01 11:22:41 +0200

    Add files via upload

---
+++ Assignment2.R
@@ -0,0 +1,25 @@
+# Assignment2: Caching the Inverse of a Matrix
+makeCacheMatrix <- function(x = matrix()){
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(solve) inv <<- solve
+  getInverse <- function() inv
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+}
+
+
+cacheInverse <- function(x, ...){
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    retrun(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setInverse(inv)
+  inv
+}
